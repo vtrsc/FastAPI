a73df39d1cd6fec8fb7f2b3714176ae8800351bc
Vitor S.C <vitorsc1998@gmail.com>
2023-03-23 17:35:30 -0300

    metodo DELETE

--- secao03/api/v1/endpoints/curso.py
+++ secao03/api/v1/endpoints/curso.py
@@ -54,7 +54,7 @@ async def get_curso(curso_id: int , db: AsyncSession =Depends(get_session)):
 
 
 # PUT Curso 
-@router.put('/curso_id', response_model=CursoSchema ,status_code=status.HTTP_202_ACCEPTED)    
+@router.put('{/curso_id}', response_model=CursoSchema ,status_code=status.HTTP_202_ACCEPTED)    
 async def put_curso(curso_id: int ,curso:CursoSchema ,db: AsyncSession= Depends(get_session)):    
      async with db as session:   
         query = select(CursoModel).filter(CursoModel.id == curso_id)    
@@ -73,3 +73,23 @@ async def put_curso(curso_id: int ,curso:CursoSchema ,db: AsyncSession= Depends(
                                 status_code=status.HTTP_404_NOT_FOUND)      
         
 
+
+# DELETE curso  
+@router.delete('/{curso_id}', response_model=CursoSchema ,status_code=status.HTTP_204_NO_CONTENT)    
+async def delete_curso(curso_id: int  ,db: AsyncSession= Depends(get_session)):    
+     async with db as session:   
+        query = select(CursoModel).filter(CursoModel.id == curso_id)    
+        result = await session.execute(query)   
+        curso_del = result.scalar_one_or_none()     
+
+        if curso_del:   
+            await session.delete(curso_del)
+
+            await session.commit()  
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:   
+            raise HTTPException(detail='curso nao encontrado',  
+                                status_code=status.HTTP_404_NOT_FOUND)      
+        
+
+    
