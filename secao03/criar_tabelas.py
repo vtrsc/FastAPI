af752e3b1a928adca75149b35871ae019d55430b
Vitor S.C <vitorsc1998@gmail.com>
2023-03-24 14:48:48 -0300

    secao04

--- secao03/criar_tabelas.py
+++ secao03/criar_tabelas.py
@@ -1,18 +1,26 @@
-from core.configs import settings   
-from core.database import engine    
+from typing import List
+
+from pydantic import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
 
 
-async def create_tables()-> None:    
-    import models.__all_models  
-    print('criando as tabelas no Banco de dados')   
+class Settings(BaseSettings):
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = 'postgresql+asyncpg://geek:university@172.30.72.61:5432/faculdade'
+    DBBaseModel = declarative_base()
 
-    async with engine.begin() as conn:  
-        await conn.run_sync(settings.DBBaseMOdel.metadata.drop_all)     
-        await conn.run_sync(settings.DBBaseMOdel.metadata.create_all)   
-        print('Tabela criada com sucesso')  
+    JWT_SECRET: str = 'qS96E1oCfq5gEZH-ngD91NC2qkcl0cffhNTIDGpF4pw'
+    """
+    import secrets
+
+    token: str = secrets.token_urlsafe(32)
+    """
+    ALGORITHM: str = 'HS256'
+    # 60 minutos * 24 horas * 7 dias => 1 semana
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    class Config:
+        case_sensitive = True
 
 
-        if __name__ == '__main__':  
-            import asyncio  
-
-            asyncio.run(create_tables())
+settings: Settings = Settings()
