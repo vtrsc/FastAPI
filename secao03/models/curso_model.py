fc8d673fb94d06f689bb691667e6594ec2bfa863
Vitor S.C <vitorsc1998@gmail.com>
2023-04-10 09:55:50 -0300

    update

--- secao03/models/curso_model.py
+++ secao03/models/curso_model.py
@@ -1,11 +1,12 @@
 from core.configs import settings   
-from sqlalchemy import Column , Integer , String    
+from sqlalchemy import Column , Integer , String        
+
 
 class CursoModel(settings.DBBaseMOdel):
-       __tabkename__ = 'cursos' 
+       __tablename__ = 'cursos' 
 
-       id: Column(Integer, primary_key=True , autoincrement=True)   
-       titulo: str= Column(String(100))    
+       id: Column(Integer, primary_key=True)   
+       titulo: str= Column(String(100))
        aulas: int = Column(Integer) 
        horas: int = Column(Integer)
                           
---
+++ secao04/core/deps.py
@@ -0,0 +1,6 @@
+from pydantic import BaseSettings   
+
+
+class  Settings(BaseSettings):  
+    API_V1_STR : '/api/v1'  
+    DB_URL: STR = 'POST'
