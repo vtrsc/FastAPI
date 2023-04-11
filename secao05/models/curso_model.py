cdc31764f178aac0a9a31793db22f442b08ab78d
Vitor S.C <vitorsc1998@gmail.com>
2023-04-11 16:54:02 -0300

    create tables

--- secao05/models/curso_model.py
+++ secao05/models/curso_model.py
@@ -1,5 +1,5 @@
-from typing import Optional 
-from sqlmodel import Field , SQLModel   
+from typing import Optional     
+from sqlmodel import Field, SQLModel
 
 class CursoModel(SQLModel ,table=True): 
     __tablename__: str= 'cursos'    
