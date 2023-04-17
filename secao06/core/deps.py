7877f353d7dd74323da227d0f3948565bafb30b6
Vitor S.C <vitorsc1998@gmail.com>
2023-04-17 15:31:29 -0300

    modulo part 2

--- secao06/core/deps.py
+++ secao06/core/deps.py
@@ -9,3 +9,49 @@ from core.auth import oauth2_schema
 from core.configs import settings
 from models.usuario_model import UsuarioModel
 
+class TokenData(BaseModel):
+    username: Optional[str] = None
+    
+async def get_session() -> Generator:
+    session: AsyncSession = Session()
+    
+    try:
+        yield session
+    finally:
+        await session.close()
+        
+        
+async def get_current_user(
+    db: Session = Depends(get_session), token: str = Depends(oauth2_schema)) -> UsuarioModel:
+    credential_exception: HTTPException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail= 'Nao foi possivel autenticar a credencial',
+        headers={"WWW-Authenticate": "Bearer"},
+        
+    )
+     
+    try:
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET,
+            algorithms=[settings.ALGORITHM],
+            options={"verify_aud": False}
+        )    
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+        
+        token_data: TokenData = TokenData(username=username)
+        
+    except JWTError:
+        raise credential_exception
+        
+        
+    async with db as session:
+        query = select(UsuarioModel).filter(UsuarioModel.id == int(token_data.username))
+        result = await session.execure(query)
+        usuario: UsuarioModel = result.scalars().unique().one_or_none()
+         
+        if usuario is None:
+         return usuario       
+            
